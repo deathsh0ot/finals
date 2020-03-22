fd62bd87bcc7afdda909ed199c1d262a090c7064
deathsh0ot <57747442+deathsh0ot@users.noreply.github.com>
2020-03-22 13:48:19 +0100

    added project to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import DesInt from './DesInt';
+import Login from './Login/Login';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+     <div>
+       <DesInt/>
+     </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="main-footer">
+    <strong>Copyright © 2019-2020 <a href="https://www.youtube.com/watch?v=ChDof6K--GI">It's A ME MARIO</a>.</strong>
+    All rights reserved.
+    <div className="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.0.2
+    </div>
+           </footer>
+                
+            </div>
+        )
+    }
+}
