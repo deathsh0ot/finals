21e9e2d2f2b4a5115e0ea7dff1d9ad0fdf79d4d9
deathsh0ot <57747442+deathsh0ot@users.noreply.github.com>
2020-03-22 14:29:43 +0100

    organizing components

---
+++ src/Designer/DesInt.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from '../Header';
+import Menu from './Menu';
+import Footer from '../Footer';
+import DesignerInterface from '../DesignerInterface';
+export default class DesInt extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Menu/>
+                <Footer/>
+            </div>
+        )
+    }
+}
